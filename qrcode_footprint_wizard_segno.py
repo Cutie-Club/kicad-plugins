4f228174d226d81e063fb8e69a90499ca72a0933
Amber Cronin <amber@amber.vision>
2022-11-30 20:08:02 +0000

    Cleanup code inline with PEP8 (#6)
    * Cleaup code inline with PEP8
    
    * Rename draw_courtyard
    
    * Resolve the parent class' inherited init function
    
    * Apply changes from review

--- qrcode_footprint_wizard_segno.py
+++ qrcode_footprint_wizard_segno.py
@@ -13,17 +13,44 @@ import pcbnew
 import FootprintWizardBase
 import segno
 
-class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
-    """FootprintWizard to create a QR Code."""
 
-    GetName = lambda self: '2D Barcode / QRCode (Segno)'
-    GetDescription = lambda self: 'QR Code generator with extended options'
-    GetReferencePrefix = lambda self: 'QR***'
-    GetValue = lambda self: self.module.Value().GetText()
+class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
+    def __init__(self):
+        super().__init__()
+        self.allow_micro_qr = True
+        self.border_auto = True
+        self.border = 4
+        self.content = 'Example'
+        self.mask_cut_out = True
+        self.negative = False
+        self.qr_code = None
+        self.pixel_size = 0.5
+        self.symbol_size = None
+        self.text_height = 1.2
+        self.text_thickness = 0.12
+        self.text_width = 1.2
+        self.use_cu = True
+        self.use_silk_s = False
+
+    def GetName(self):
+        """Return the name of the footprint wizard."""
+        return '2D Barcode / QRCode (Segno)'
+
+    def GetDescription(self):
+        """Return the footprint wizard description."""
+        return 'QR Code generator with extended options'
+
+    def GetReferencePrefix(self):
+        """Return the footprint reference designator prefix."""
+        return 'QR***'
+
+    def GetValue(self):
+        """Return the value (name) of the generated footprint."""
+        return self.module.Value().GetText()
 
     def GenerateParameterList(self):
         """Generate parameter list."""
-        self.AddParam("Barcode", "Pixel Width", self.uMM, 0.5, min_value=0.01)
+        self.AddParam("Barcode", "Pixel Size", self.uMM, 0.5, min_value=0.01)
         self.AddParam("Barcode", "Border Auto", self.uBool, True)
         self.AddParam("Barcode", "Border", self.uInteger, 4)
         self.AddParam("Barcode", "Contents", self.uString, 'Example')
@@ -38,80 +65,79 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         self.AddParam("Caption", "Thickness", self.uMM, 0.12)
 
     def CheckParameters(self):
-        """Check parameter."""
-        self.Barcode = str(self.parameters['Barcode']['Contents'])
-        self.X = self.parameters['Barcode']['Pixel Width']
+        """Check parameters."""
+        self.content = str(self.parameters['Barcode']['Contents'])
+        self.pixel_size = self.parameters['Barcode']['Pixel Size']
         self.negative = self.parameters['Barcode']['Negative']
-        self.UseSilkS = self.parameters['Barcode']['Use SilkS layer']
-        self.UseCu = self.parameters['Barcode']['Use Cu layer']
-        self.MaskCutOut = self.parameters['Barcode']['Mask CutOut']
+        self.use_silk_s = self.parameters['Barcode']['Use SilkS layer']
+        self.use_cu = self.parameters['Barcode']['Use Cu layer']
+        self.mask_cut_out = self.parameters['Barcode']['Mask CutOut']
 
         self.border_auto = self.parameters['Barcode']['Border Auto']
         self.border = int(self.parameters['Barcode']['Border'])
         self.allow_micro_qr = self.parameters['Barcode']['Allow Micro QR']
 
-        self.textHeight = int(self.parameters['Caption']['Height'])
-        self.textThickness = int(self.parameters['Caption']['Thickness'])
-        self.textWidth = int(self.parameters['Caption']['Width'])
-        self.module.Value().SetText(str(self.Barcode))
+        self.text_height = int(self.parameters['Caption']['Height'])
+        self.text_thickness = int(self.parameters['Caption']['Thickness'])
+        self.text_width = int(self.parameters['Caption']['Width'])
+        self.module.Value().SetText(str(self.content))
 
         # Build Qrcode
-        # use all default options
-        if self.allow_micro_qr:
-            self.qr = segno.make(str(self.Barcode))
-        else:
-            self.qr = segno.make_qr(str(self.Barcode))
+        self.qr_code = segno.make(str(self.content), micro=self.allow_micro_qr)
 
         if self.border_auto:
-            self.parameters['Barcode']['Border'] = self.qr.default_border_size
+            self.parameters['Barcode']['Border'] = self.qr_code.default_border_size
             # None means automatic border width
             self.border = None
 
-        self.symbol_size = self.qr.symbol_size(border=self.border)
+        self.symbol_size = self.qr_code.symbol_size(border=self.border)
 
-    def drawSquareArea(self, layer, size, xposition, yposition, line_width=0):
+    def __draw_square_area(
+      self,
+      layer,
+      size,
+      x_position,
+      y_position,
+      line_width=0
+      ):
         """Draw square area."""
         # prepare values
         # 0,000005mm == 5
+        x_position = int(x_position)
+        y_position = int(y_position)
         line_width = int(line_width * 1000)
-        xposition = int(xposition)
-        yposition = int(yposition)
+
         # creates a filled FP_SHAPE of polygon type. The polygon is a square
-        # this could possibly be replaced with
         polygon = pcbnew.FP_SHAPE(self.module)
         polygon.SetShape(pcbnew.SHAPE_T_POLY)
         polygon.SetWidth(line_width)
         polygon.SetLayer(layer)
         halfsize = int(size / 2)
         polygon.GetPolyShape().NewOutline()
-        polygon.GetPolyShape().Append(
-            halfsize + xposition, halfsize + yposition)
-        polygon.GetPolyShape().Append(
-            halfsize + xposition, -halfsize + yposition)
-        polygon.GetPolyShape().Append(
-            - halfsize + xposition, -halfsize + yposition)
-        polygon.GetPolyShape().Append(
-            - halfsize + xposition, halfsize + yposition)
+        polygon.GetPolyShape().Append(halfsize + x_position, halfsize + y_position)
+        polygon.GetPolyShape().Append(halfsize + x_position, -halfsize + y_position)
+        polygon.GetPolyShape().Append(-halfsize + x_position, -halfsize + y_position)
+        polygon.GetPolyShape().Append(-halfsize + x_position, halfsize + y_position)
         polygon.SetFilled(True)
         return polygon
 
-    def _drawPixel(self, xposition, yposition):
+    def __draw_pixel(self, x_position, y_position):
         # build a square polygon on required layers
-        if self.UseCu:
-            polygon = self.drawSquareArea(
-                pcbnew.F_Cu, self.X, xposition, yposition)
+        if self.use_cu:
+            polygon = self.__draw_square_area(
+                pcbnew.F_Cu, self.pixel_size, x_position, y_position)
             self.module.Add(polygon)
 
-        if self.UseSilkS:
-            polygon = self.drawSquareArea(
-                pcbnew.F_SilkS, self.X, xposition, yposition)
+        if self.use_silk_s:
+            polygon = self.__draw_square_area(
+                pcbnew.F_SilkS, self.pixel_size, x_position, y_position)
             self.module.Add(polygon)
 
-    def _add_MaskCutOut(self):
+    def __add_mask_cutout(self):
         pad = pcbnew.PAD(self.module)
         pad.SetSize(pcbnew.wxSize(
-            self.X * self.symbol_size[0],
-            self.X * self.symbol_size[1]))
+            self.pixel_size * self.symbol_size[0],
+            self.pixel_size * self.symbol_size[1]))
         pad.SetPosition(pcbnew.wxPoint(0, 0))
         pad.SetShape(pcbnew.PAD_SHAPE_RECT)
         pad.SetAttribute(pcbnew.PAD_ATTRIB_SMD)
@@ -121,12 +147,14 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         pad.SetLayerSet(layerset)
         self.module.Add(pad)
 
-    def _draw_coutyard(self):
+    def __draw_courtyard(self):
         """Draw Courtyard."""
         # https://klc.kicad.org/footprint/f5/f5.3/
-        size = self.X * self.symbol_size[0]
+        size = self.pixel_size * self.symbol_size[0]
+
         # add clearance
         size += pcbnew.FromMM(0.25 * 2)
+
         # Round courtyard positions to 0.1 mm,
         # rectangle will thus land on a 0.05mm grid
         pcbnew.PutOnGridMM(size, pcbnew.FromMM(0.10))
@@ -140,12 +168,12 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         """Build this Footprint."""
         # used many times...
         half_number_of_elements = self.symbol_size[0] / 2
-        half_element_size = self.X/2
+        half_element_size = self.pixel_size/2
 
         # Center position of QrCode
-        yposition = - int(half_number_of_elements * self.X - half_element_size)
-        for line in self.qr.matrix_iter(border=self.border):
-            xposition = - int(half_number_of_elements * self.X - half_element_size)
+        y_position = - int(half_number_of_elements * self.pixel_size - half_element_size)
+        for line in self.qr_code.matrix_iter(border=self.border):
+            x_position = - int(half_number_of_elements * self.pixel_size - half_element_size)
             for pixel in line:
                 # Trust table for drawing a pixel
                 # Negative is a boolean;
@@ -157,27 +185,30 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
                 #        1 |     1 | 0
                 # => Draw as Xor
                 if self.negative != pixel:  # Xor...
-                    self._drawPixel(xposition, yposition)
-                xposition += self.X
-            yposition += self.X
+                    self.__draw_pixel(x_position, y_position)
+                x_position += self.pixel_size
+            y_position += self.pixel_size
+
         # add mask cutout
-        if self.UseCu and self.MaskCutOut:
-            self._add_MaskCutOut()
-        self._draw_coutyard()
+        if self.use_cu and self.mask_cut_out:
+            self.__add_mask_cutout()
+        self.__draw_courtyard()
+
         # add labels
-        # int((5 + half_number_of_elements) * self.X))
-        textPosition = int(
-            (self.textHeight) + ((1 + half_number_of_elements) * self.X))
-        self.module.Value().SetPosition(pcbnew.wxPoint(0, - textPosition))
-        self.module.Value().SetTextHeight(self.textHeight)
-        self.module.Value().SetTextWidth(self.textWidth)
-        self.module.Value().SetTextThickness(self.textThickness)
+        # int((5 + half_number_of_elements) * self.pixel_size))
+        text_position = int(
+            (self.text_height) + ((1 + half_number_of_elements) * self.pixel_size))
+        self.module.Value().SetPosition(pcbnew.wxPoint(0, - text_position))
+        self.module.Value().SetTextHeight(self.text_height)
+        self.module.Value().SetTextWidth(self.text_width)
+        self.module.Value().SetTextThickness(self.text_thickness)
         self.module.Value().SetLayer(pcbnew.F_Fab)
 
-        self.module.Reference().SetPosition(pcbnew.wxPoint(0, textPosition))
-        self.module.Reference().SetTextHeight(self.textHeight)
-        self.module.Reference().SetTextWidth(self.textWidth)
-        self.module.Reference().SetTextThickness(self.textThickness)
+        self.module.Reference().SetPosition(pcbnew.wxPoint(0, text_position))
+        self.module.Reference().SetTextHeight(self.text_height)
+        self.module.Reference().SetTextWidth(self.text_width)
+        self.module.Reference().SetTextThickness(self.text_thickness)
         self.module.Reference().SetLayer(pcbnew.F_Fab)
+
 
 QRCodeWizardSegno().register()
