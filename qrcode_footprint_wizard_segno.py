ef088797c768dfbf9f53f52a4076779cc304bc6c
Amber Cronin <amber@amber.vision>
2023-04-15 16:52:09 +0100

    Fix QR Code Footprint Wizard (Segno) for KiCad 7.0 (#15)
    * 14: Fix QR Code Footprint Wizard (Segno) for KiCad 7.0
    
    - Changed calls to wx* classes with pcbnew.VECTOR2I
    - Added Python Black pre-commit hook to ensure good formatting :)
    - Run pre-commit hooks
    
    * 14: Fix allow_micro_qr

--- qrcode_footprint_wizard_segno.py
+++ qrcode_footprint_wizard_segno.py
@@ -20,7 +20,7 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         self.allow_micro_qr = True
         self.border_auto = True
         self.border = 4
-        self.content = 'Example'
+        self.content = "Example"
         self.mask_cut_out = True
         self.negative = False
         self.qr_code = None
@@ -34,15 +34,15 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
 
     def GetName(self):
         """Return the name of the footprint wizard."""
-        return '2D Barcode / QRCode (Segno)'
+        return "2D Barcode / QRCode (Segno)"
 
     def GetDescription(self):
         """Return the footprint wizard description."""
-        return 'QR Code generator with extended options'
+        return "QR Code generator with extended options"
 
     def GetReferencePrefix(self):
         """Return the footprint reference designator prefix."""
-        return 'QR***'
+        return "QR***"
 
     def GetValue(self):
         """Return the value (name) of the generated footprint."""
@@ -53,7 +53,7 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         self.AddParam("Barcode", "Pixel Size", self.uMM, 0.5, min_value=0.01)
         self.AddParam("Barcode", "Border Auto", self.uBool, True)
         self.AddParam("Barcode", "Border", self.uInteger, 4)
-        self.AddParam("Barcode", "Contents", self.uString, 'Example')
+        self.AddParam("Barcode", "Contents", self.uString, "Example")
         self.AddParam("Barcode", "Allow Micro QR", self.uBool, True)
         self.AddParam("Barcode", "Negative", self.uBool, False)
         self.AddParam("Barcode", "Use SilkS layer", self.uBool, False)
@@ -66,40 +66,35 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
 
     def CheckParameters(self):
         """Check parameters."""
-        self.content = str(self.parameters['Barcode']['Contents'])
-        self.pixel_size = self.parameters['Barcode']['Pixel Size']
-        self.negative = self.parameters['Barcode']['Negative']
-        self.use_silk_s = self.parameters['Barcode']['Use SilkS layer']
-        self.use_cu = self.parameters['Barcode']['Use Cu layer']
-        self.mask_cut_out = self.parameters['Barcode']['Mask CutOut']
+        self.content = str(self.parameters["Barcode"]["Contents"])
+        self.pixel_size = self.parameters["Barcode"]["Pixel Size"]
+        self.negative = self.parameters["Barcode"]["Negative"]
+        self.use_silk_s = self.parameters["Barcode"]["Use SilkS layer"]
+        self.use_cu = self.parameters["Barcode"]["Use Cu layer"]
+        self.mask_cut_out = self.parameters["Barcode"]["Mask CutOut"]
 
-        self.border_auto = self.parameters['Barcode']['Border Auto']
-        self.border = int(self.parameters['Barcode']['Border'])
-        self.allow_micro_qr = self.parameters['Barcode']['Allow Micro QR']
+        self.border_auto = self.parameters["Barcode"]["Border Auto"]
+        self.border = int(self.parameters["Barcode"]["Border"])
+        self.allow_micro_qr = self.parameters["Barcode"]["Allow Micro QR"]
 
-        self.text_height = int(self.parameters['Caption']['Height'])
-        self.text_thickness = int(self.parameters['Caption']['Thickness'])
-        self.text_width = int(self.parameters['Caption']['Width'])
+        self.text_height = int(self.parameters["Caption"]["Height"])
+        self.text_thickness = int(self.parameters["Caption"]["Thickness"])
+        self.text_width = int(self.parameters["Caption"]["Width"])
         self.module.Value().SetText(str(self.content))
 
         # Build Qrcode
-        self.qr_code = segno.make(str(self.content), micro=self.allow_micro_qr)
+        self.qr_code = segno.make(
+            str(self.content), micro=False if self.allow_micro_qr == False else None
+        )
 
         if self.border_auto:
-            self.parameters['Barcode']['Border'] = self.qr_code.default_border_size
+            self.parameters["Barcode"]["Border"] = self.qr_code.default_border_size
             # None means automatic border width
             self.border = None
 
         self.symbol_size = self.qr_code.symbol_size(border=self.border)
 
-    def __draw_square_area(
-      self,
-      layer,
-      size,
-      x_position,
-      y_position,
-      line_width=0
-      ):
+    def __draw_square_area(self, layer, size, x_position, y_position, line_width=0):
         """Draw square area."""
         # prepare values
         # 0,000005mm == 5
@@ -125,20 +120,25 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         # build a square polygon on required layers
         if self.use_cu:
             polygon = self.__draw_square_area(
-                pcbnew.F_Cu, self.pixel_size, x_position, y_position)
+                pcbnew.F_Cu, self.pixel_size, x_position, y_position
+            )
             self.module.Add(polygon)
 
         if self.use_silk_s:
             polygon = self.__draw_square_area(
-                pcbnew.F_SilkS, self.pixel_size, x_position, y_position)
+                pcbnew.F_SilkS, self.pixel_size, x_position, y_position
+            )
             self.module.Add(polygon)
 
     def __add_mask_cutout(self):
         pad = pcbnew.PAD(self.module)
-        pad.SetSize(pcbnew.wxSize(
-            self.pixel_size * self.symbol_size[0],
-            self.pixel_size * self.symbol_size[1]))
-        pad.SetPosition(pcbnew.wxPoint(0, 0))
+        pad.SetSize(
+            pcbnew.VECTOR2I(
+                self.pixel_size * self.symbol_size[0],
+                self.pixel_size * self.symbol_size[1],
+            )
+        )
+        pad.SetPosition(pcbnew.VECTOR2I(0, 0))
         pad.SetShape(pcbnew.PAD_SHAPE_RECT)
         pad.SetAttribute(pcbnew.PAD_ATTRIB_SMD)
         pad.SetName("")
@@ -168,12 +168,14 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         """Build this Footprint."""
         # used many times...
         half_number_of_elements = self.symbol_size[0] / 2
-        half_element_size = self.pixel_size/2
+        half_element_size = self.pixel_size / 2
 
         # Center position of QrCode
-        y_position = - int(half_number_of_elements * self.pixel_size - half_element_size)
+        y_position = -int(half_number_of_elements * self.pixel_size - half_element_size)
         for line in self.qr_code.matrix_iter(border=self.border):
-            x_position = - int(half_number_of_elements * self.pixel_size - half_element_size)
+            x_position = -int(
+                half_number_of_elements * self.pixel_size - half_element_size
+            )
             for pixel in line:
                 # Trust table for drawing a pixel
                 # Negative is a boolean;
@@ -197,14 +199,15 @@ class QRCodeWizardSegno(FootprintWizardBase.FootprintWizard):
         # add labels
         # int((5 + half_number_of_elements) * self.pixel_size))
         text_position = int(
-            (self.text_height) + ((1 + half_number_of_elements) * self.pixel_size))
-        self.module.Value().SetPosition(pcbnew.wxPoint(0, - text_position))
+            (self.text_height) + ((1 + half_number_of_elements) * self.pixel_size)
+        )
+        self.module.Value().SetPosition(pcbnew.VECTOR2I(0, -text_position))
         self.module.Value().SetTextHeight(self.text_height)
         self.module.Value().SetTextWidth(self.text_width)
         self.module.Value().SetTextThickness(self.text_thickness)
         self.module.Value().SetLayer(pcbnew.F_Fab)
 
-        self.module.Reference().SetPosition(pcbnew.wxPoint(0, text_position))
+        self.module.Reference().SetPosition(pcbnew.VECTOR2I(0, text_position))
         self.module.Reference().SetTextHeight(self.text_height)
         self.module.Reference().SetTextWidth(self.text_width)
         self.module.Reference().SetTextThickness(self.text_thickness)
